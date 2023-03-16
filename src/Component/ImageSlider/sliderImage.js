e59fd321a7051715bc63b09c431eda641221fcb8
Utkarsh721 <utkarshrai2121@gmail.com>
2023-03-16 23:08:26 +0530

    Second

---
+++ src/Component/ImageSlider/sliderImage.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import banner1 from "../../assets/banner1.jpg"
+import banner2 from "../../assets/banner2.jpg";
+import banner3 from "../../assets/banner3.jpg";
+
+export default [
+  {
+    urls: banner1,
+  },
+  {
+    urls: banner2,
+  },
+  {
+    urls: banner3,
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+
+import Slider from "../Component/ImageSlider/Slider";
+
 
 const Home = () => {
-  return (
-    <div><h1>Home</h1></div>
-  )
+    return(
+<div>
+<Slider/>
+</div>
+    )
 }
 
-export default Home
+export default Home;
